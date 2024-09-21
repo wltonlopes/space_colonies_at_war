ac2cd7f23d625b1c008d10c543e13f3740999160
Welton Lopes <39802614+wltonlopes@users.noreply.github.com>
2024-09-21 12:25:09 -0300

    t

---
+++ gui/session/session_DE.js
@@ -0,0 +1,16 @@
+var standardInit = init;
+init = function(initData, hotloadData)
+{
+	standardInit(initData, hotloadData);
+	initCivChoicesDialog();
+}
+
+/**
+ * Unit classes to be checked for the idle-worker-hotkey.
+ */
+var g_WorkerTypes = ["IdleWorker"];
+
+/**
+ * Order in which the panel entities are shown.
+ */
+var g_PanelEntityOrder = ["Hero", "Minister", "Relic"];
